8324fd981e29bc63386d6e81bea3c54787237ef2
Karuppiah Natarajan <karuppiah7890@gmail.com>
2018-11-24 09:27:47 +0530

    Disable default /vagrant directory sharing

---
+++ README.md
@@ -0,0 +1,3 @@
+# postgres-vagrant
+
+Followed [chapter 16](https://www.postgresql.org/docs/current/installation.html) to install postgres 11 in vagrant
---
+++ install-postgres.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt install -y make gcc libreadline-dev zlib1g-dev
+cd /vagrant/
+wget https://ftp.postgresql.org/pub/source/v11.1/postgresql-11.1.tar.gz
+wget https://ftp.postgresql.org/pub/source/v11.1/postgresql-11.1.tar.gz.sha256
+shasum -c postgresql-11.1.tar.gz.sha256
+gunzip postgresql-11.1.tar.gz
+tar xf postgresql-11.1.tar
+cd postgresql-11.1
+./configure
+make all
+make install
+export PATH=/usr/local/pgsql/bin:$PATH
